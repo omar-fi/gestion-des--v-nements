f9c1f332d3e30a9fce35bbe463b503125dd1e8f7
Hassan ENNACIRI CHERKAOUI <ennaciricherkaouih@gmail.com>
2025-05-26 13:19:51 +0100

    v10

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD9GcvpZUhsUXY4LqXfnqhWoG4tRr8AMXY",
+    authDomain: "eventhub-b5162.firebaseapp.com",
+    projectId: "eventhub-b5162",
+    storageBucket: "eventhub-b5162.appspot.com", // ⚠️ CORRIGÉ ".app" → ".app**spot**.com"
+    messagingSenderId: "286799364258",
+    appId: "1:286799364258:web:cce6e396b1ac35cb4617fb",
+    measurementId: "G-TB52X6PHSN"
+  }
+};
